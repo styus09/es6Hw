e2b81f87fdd4df9ca72cb8c6fb1e33a7947157b2
Sean <styus09@gmail.com>
2021-10-20 11:32:53 -0400

    es6Hw

---
+++ app.js
@@ -0,0 +1,49 @@
+var num = 9
+var num2 = 26
+
+function addTwo(num, num2) {
+    console.log(num + num2);
+}
+
+addTwo(num, num2)
+
+const addTwo = (num, num2) => {
+    console.log(num + num2);
+}
+
+addTwo(num, num2)
+
+
+
+
+function subtractTwo(num , num2) {
+    console.log(num - num2)
+}
+subtractTwo(num,num2)
+
+const subtractTwo = (num , num2) => {
+    console.log(num - num2)
+}
+subtractTwo(num,num2)
+
+
+function multiplyTwo(num , num2) {
+    console.log(num * num2)
+}
+multiplyTwo(num,num2)
+
+const multiplyTwo = (num , num2) => {
+    console.log(num * num2)
+}
+multiplyTwo(num,num2)
+
+
+function divideTwo(num , num2) {
+    console.log(num / num2)
+}
+divideTwo(num,num2)
+
+const divideTwo = (num , num2) => {
+    console.log(num / num2)
+}
+divideTwo(num,num2)
